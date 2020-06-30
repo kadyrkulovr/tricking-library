f1fe6f8e24b9a21d9816ff044dc4664823d8e34c
Ruslan Kadyrkulov <r.kadyrkulov@gmail.com>
2020-06-30 14:25:06 +0600

    init

---
+++ TrickingLibrary.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class HomeController : ControllerBase
+    {
+        public string Index()
+        {
+            return "Hello world";
+        }
+    }
+}
